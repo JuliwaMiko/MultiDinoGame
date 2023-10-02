44e2e45651ddc60207d21dd6321ad4f5b05da2d3
ScandieFox <Nikolai.foxdal@gmail.com>
2023-10-02 12:29:02 +0200

    jump fixed

--- MultiDinoGame/Assets/Scripts & Drips/PlayerMovement.cs
+++ MultiDinoGame/Assets/Scripts & Drips/PlayerMovement.cs
@@ -21,7 +21,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("Space") && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(Vector2.up * jump);
         }
