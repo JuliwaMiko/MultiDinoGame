ab7a786984b5cb0b431af6d3650a25ab3f77312a
cpu-man <sidneythyaganielsen@gmail.com>
2023-10-09 10:49:47 +0200

    k

--- MultiDinoGame/Assets/Scripts & Drips/PlayerMovement.cs
+++ MultiDinoGame/Assets/Scripts & Drips/PlayerMovement.cs
@@ -14,11 +14,6 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
