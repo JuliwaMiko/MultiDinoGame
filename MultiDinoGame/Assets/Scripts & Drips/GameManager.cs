f23a53a65a0dcbce7d239cdd23085d2362965c26
ScandieFox <Nikolai.foxdal@gmail.com>
2023-10-09 11:01:25 +0200

    GameManager

---
+++ MultiDinoGame/Assets/Scripts & Drips/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool isPlayerDestroyed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerDestroyed)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+    public void SetPlayerDestroyed(bool destroyed)
+    {
+        isPlayerDestroyed = destroyed;
+    }
+}
