398805901e5d7c3d6a36f6608e222470fb9464e4
ScandieFox <Nikolai.foxdal@gmail.com>
2023-10-09 11:30:19 +0200

    more spawners

--- MultiDinoGame/Assets/Scripts & Drips/Spawner.cs
+++ MultiDinoGame/Assets/Scripts & Drips/Spawner.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    float waitTime = 3;
     public GameObject EnemyPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTimer());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    IEnumerator SpawnTimer()
+    {
+        waitTime = Random.Range(1f, 3f);
+        yield return new WaitForSeconds(waitTime);
+        Instantiate(EnemyPrefab, transform.position, Quaternion.identity);
+        StartCoroutine(SpawnTimer());
     }
 }
