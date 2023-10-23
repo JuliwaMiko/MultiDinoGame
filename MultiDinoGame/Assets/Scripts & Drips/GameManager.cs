ad9f8b5554067ce22e75a47140b4d0c7df6ff66b
ScandieFox <Nikolai.foxdal@gmail.com>
2023-10-23 11:01:34 +0200

    Score text 0.1

--- MultiDinoGame/Assets/Scripts & Drips/GameManager.cs
+++ MultiDinoGame/Assets/Scripts & Drips/GameManager.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public TMP_Text player2ScoreText;
+    public TMP_Text player1ScoreText;
     private bool isPlayerDestroyed = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player1ScoreText.text = "Score";
+        player2ScoreText.text = "Score";
     }
 
     // Update is called once per frame
