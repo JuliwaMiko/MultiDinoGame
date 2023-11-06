7c78b70fcffab72cb520f8245bee62a39e80b722
ScandieFox <Nikolai.foxdal@gmail.com>
2023-11-06 15:03:43 +0100

    updated the spawner

--- MultiDinoGame/Assets/Scripts & Drips/Spawner.cs
+++ MultiDinoGame/Assets/Scripts & Drips/Spawner.cs
@@ -7,12 +7,16 @@ public class Spawner : MonoBehaviour
     float waitTime = 5;
     public GameObject EnemyPrefab;
     public GameObject[] SpawnPoints;
+    public GameObject[] ExtraSpawn;
     int SpawnPointIndex = 0;
+    int ExtraSpawnIndex = 0;
     float speedMultiplier = 2;
+    float airSpawnRatio = 0.3f;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTimer());
+        StartCoroutine(AirSpawn());
     }
 
     // Update is called once per frame
@@ -24,9 +28,20 @@ public class Spawner : MonoBehaviour
     {
         waitTime = Random.Range(3f, 4f);
         yield return new WaitForSeconds(waitTime);
-        SpawnPointIndex = Random.Range(0, 3);
-        SpawnObjectScript sp = Instantiate(EnemyPrefab, SpawnPoints[SpawnPointIndex]. transform.position, Quaternion.identity).GetComponent<SpawnObjectScript>();
+        // Randomly select between ground and air spawn points based on the given ratio
+        SpawnPointIndex = Random.Range(0, SpawnPoints.Length);
+        SpawnObjectScript sp = Instantiate(EnemyPrefab, SpawnPoints[SpawnPointIndex].transform.position, Quaternion.identity).GetComponent<SpawnObjectScript>();
         sp.speed = speedMultiplier;
+
         StartCoroutine(SpawnTimer());
+    }
+    IEnumerator AirSpawn()
+    {
+        waitTime = Random.Range(6f, 10f);
+        yield return new WaitForSeconds(waitTime);
+        SpawnPointIndex = Random.Range(0, ExtraSpawn.Length);
+        SpawnObjectScript sp = Instantiate(EnemyPrefab, ExtraSpawn[SpawnPointIndex].transform.position, Quaternion.identity).GetComponent<SpawnObjectScript>();
+        sp.speed = speedMultiplier;
+        StartCoroutine(AirSpawn());
     }
 }
